fd2a378b7c8b2fd0e8337c4e475607c11ae26a6d
Carla Cristine <carlacs.cristine@gmail.com>
2024-01-11 15:22:39 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { ThemeProvider } from "styled-components"
 import { defaultTheme } from "./themes/default"
 import { GlobalStyle } from "./styles/global"
+import { Transactions } from "./pages/Transactions"
 
  export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle/>
-      
-      <h1>Hello </h1>
+
+     <Transactions/>
     </ThemeProvider>
   )
 }
